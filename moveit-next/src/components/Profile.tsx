27346cad11709618276e9f3ee0bad6f2f9476126
ro-prates <ronaldy1995@hotmail.com>
2021-04-14 14:58:21 -0300

    melhorando a aparência e deixando o timer pronto

--- moveit-next/src/components/Profile.tsx
+++ moveit-next/src/components/Profile.tsx
@@ -6,7 +6,10 @@ export function Profile(){
             <img src="https://github.com/ro-prates.png" alt="Ronaldy Prates"/>
             <div>
                 <strong>Ronaldy Prates</strong>
-                <p>Level 1</p>
+                <p>
+                    <img src="icons/level.svg" alt="Level"/>
+                    Level 1
+                </p>
             </div>
         </div>
     );
