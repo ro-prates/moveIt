8f3dfb65655987289eefe682a3a834a53d5ecb28
ro-prates <ronaldy1995@hotmail.com>
2021-04-20 14:25:16 -0300

    alterando valores estáticos para dinâmicos

--- moveit-next/src/components/Countdown.tsx
+++ moveit-next/src/components/Countdown.tsx
@@ -1,10 +1,11 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Countdown.module.css';
 
 let countdownTimeout: NodeJS.Timeout;
 
 export function Countdown(){
-
+    const { startNewChallenge } = useContext(ChallengesContext);
     const [time, setTime] = useState(0.1 * 60)
     const minutes = Math.floor(time / 60);
     const seconds = time % 60;
@@ -23,7 +24,6 @@ export function Countdown(){
         setTime(0.1 * 60);
     }
 
-
     useEffect(() => {
         if(isActive && time > 0){
             countdownTimeout = setTimeout(() => {
@@ -32,6 +32,7 @@ export function Countdown(){
         }else if(isActive && time === 0){
             setHasFinished(true);
             setIsActive(false);
+            startNewChallenge();
         }
     }, [isActive, time])
 
